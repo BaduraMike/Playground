793c1250c8ada3306c32a6b35bfb194a88d4bcfc
Michal Badura <badura.mike@gmail.com>
2019-04-16 22:10:23 +0200

    Add TodoItem.

---
+++ todoapp-react/src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class TodoItem extends Component {
+    render() {
+        return (
+            <div>
+                <p>{ this.props.todo.title }</p>
+            </div>
+        )
+    }
+}
+export default TodoItem;
