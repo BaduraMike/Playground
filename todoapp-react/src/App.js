ec9cb0c86b4be499676381b9c4d5b106043b845d
Michal Badura <badura.mike@gmail.com>
2019-04-21 22:11:31 +0200

    Add delete Todo.

--- todoapp-react/src/App.js
+++ todoapp-react/src/App.js
@@ -23,20 +23,29 @@ class App extends Component {
         ]
     };
 
+    //Toggle Complete
     markComplete = (id) => {
-        this.setState( {todos: this.state.todos.map(todo => {
-            if(todo.id === id) {
-                todo.completed = !todo.completed
-            }
-            return todo;
-            })} );
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo;
+            })
+        });
+    };
+
+    //Delete Todo
+    delTodo = (id) => {
+        this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]})
     };
 
     render() {
         return (
             <div className="App">
                 <h1>ToDoApp</h1>
-                <Todos todos={this.state.todos} markComplete={this.markComplete}/>
+                <Todos todos={this.state.todos} markComplete={this.markComplete}
+                       delTodo={this.delTodo}/>
             </div>
         );
     }
