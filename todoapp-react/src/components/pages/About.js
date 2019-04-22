d8eab53515d304a847d14b8183175faecd6b27f9
Michal Badura <badura.mike@gmail.com>
2019-04-22 21:40:45 +0200

    Add About and its Route.

---
+++ todoapp-react/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>
+                This is the TodoList training app.
+                It is a part of a React Crash Course.
+            </p>
+        </React.Fragment>
+    )
+}
+
+export default About;
