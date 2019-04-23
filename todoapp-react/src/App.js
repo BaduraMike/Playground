8faf819697bb70cb9b873e399ee7b6b1186b0305
Michal Badura <badura.mike@gmail.com>
2019-04-23 20:28:34 +0200

    Add axios.post(). Post Todo to "https://jsonplaceholder.typicode.com/todos".

--- todoapp-react/src/App.js
+++ todoapp-react/src/App.js
@@ -4,7 +4,7 @@ import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import About from './components/pages/About';
-import uuid from 'uuid';
+// import uuid from 'uuid';
 import axios from 'axios';
 
 import './App.css';
@@ -38,12 +38,12 @@ class App extends Component {
 
     //Add Todo
     addTodo = (title) => {
-        const newTodo = {
-            id: uuid.v4(),
-            title: title,
-            completed: false
-        };
-        this.setState({todos: [...this.state.todos, newTodo]});
+        axios.post('https://jsonplaceholder.typicode.com/todos',
+            {
+                title: title,
+                completed: false
+            }).then(res => this.setState({todos:
+                [...this.state.todos, res.data]}));
     };
 
     render() {
