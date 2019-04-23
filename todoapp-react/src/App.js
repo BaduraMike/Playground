63f698bfe514ef7bf32c327b44f6d95909bcc56f
Michal Badura <badura.mike@gmail.com>
2019-04-23 20:15:43 +0200

    Add axios.get(). Get Todos from "https://jsonplaceholder.typicode.com/todos".

--- todoapp-react/src/App.js
+++ todoapp-react/src/App.js
@@ -5,28 +5,19 @@ import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import About from './components/pages/About';
 import uuid from 'uuid';
+import axios from 'axios';
+
 import './App.css';
 
 class App extends Component {
     state = {
-        todos: [
-            {
-                id: uuid.v4(),
-                title: 'Learn React',
-                completed: false
-            },
-            {
-                id: uuid.v4(),
-                title: 'Eat',
-                completed: false
-            },
-            {
-                id: uuid.v4(),
-                title: 'Sleep',
-                completed: false
-            },
-        ]
+        todos: []
     };
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+            .then(res => this.setState({todos: res.data}))
+    }
 
     //Toggle Complete
     markComplete = (id) => {
